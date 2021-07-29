64f1d0974eaf465b16f28f5159f5083779018c77
submit666 <80131350+submit666@users.noreply.github.com>
2021-07-29 15:11:02 +0800

    Update jd_sgmh 2.js

--- jd_sgmh 2.js
+++ jd_sgmh 2.js
@@ -29,8 +29,8 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let appId = '1EFRXxg' , homeDataFunPrefix = 'interact_template', collectScoreFunPrefix = 'harmony', message = ''
 let lotteryResultFunPrefix = homeDataFunPrefix, browseTime = 6
 const inviteCodes = [
-  'T019-aknAFRllhyoQlyI46gCjVQmoaT5kRrbA@T010_aU6SR8Q_QCjVQmoaT5kRrbA@T0225KkcRhcbp1CBJhv0wfZedQCjVQmoaT5kRrbA@T027Zm_olqSxIOtH97BATGmKoWraLawCjVQmoaT5kRrbA',
-  'T019-aknAFRllhyoQlyI46gCjVQmoaT5kRrbA@T010_aU6SR8Q_QCjVQmoaT5kRrbA@T027Zm_olqSxIOtH97BATGmKoWraLawCjVQmoaT5kRrbA@T0225KkcRk1N_FeCJhv3xvdfcQCjVQmoaT5kRrbA'
+  'T0225KkcR0gZ9lSGch3zkvQPcACjVQmoaT5kRrbA@T0225KkcRx8b8FfVdBugkvNZcwCjVQmoaT5kRrbA@T0205KkcJ0VmsyqhcX6SwbBiCjVQmoaT5kRrbA@T0205KkcA3xsnAqLaW699ZVJCjVQmoaT5kRrbA',
+  'T0225KkcR0gZ9lSGch3zkvQPcACjVQmoaT5kRrbA@T0225KkcRx8b8FfVdBugkvNZcwCjVQmoaT5kRrbA@T0205KkcJ0VmsyqhcX6SwbBiCjVQmoaT5kRrbA@T0205KkcA3xsnAqLaW699ZVJCjVQmoaT5kRrbA'
 ];
 const randomCount = $.isNode() ? 20 : 5;
 const notify = $.isNode() ? require('./sendNotify') : '';
